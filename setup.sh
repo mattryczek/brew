308d2002f98d6c4c3b9212d9a5b533649a76f232
Mateusz Ryczek <7564427+mattryczek@users.noreply.github.com>
2021-01-25 18:05:38 -0600

    Add stealth options

--- setup.sh
+++ setup.sh
@@ -3,6 +3,10 @@
 # Turn off file prompt for textedit and just open a fresh doc
 defaults write com.apple.TextEdit NSShowAppCentricOpenPanelInsteadOfUntitledFile -bool false
 
+# Turn off .DS_Store creation on USB and network drives
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
+
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 curl -o /tmp/Brewfile https://raw.githubusercontent.com/mattryczek/brew/main/Brewfile 
